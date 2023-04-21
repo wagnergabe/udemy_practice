3da02c93e4ae6da2c08f8eb389faaeb77059ab7e
wagnergabe <102180936+wagnergabe@users.noreply.github.com>
2023-04-20 23:39:44 -0500

    create second component/date

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,10 +1,12 @@
+import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
 
+
   return (
     <div className="expense-item">
-      <div>{props.date}</div>
+        <ExpenseDate date = {props.date}/>
       <div className= "expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">{props.amount}</div>
