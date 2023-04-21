6d40f11154bd35d10058d4fc5eee7a01199a5a13
wagnergabe <102180936+wagnergabe@users.noreply.github.com>
2023-04-20 22:27:13 -0500

    create expense prop

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,15 +1,18 @@
 import "./ExpenseItem.css";
 
-function ExpenseItem() {
+function ExpenseItem(props) {
+
   return (
     <div className="expense-item">
-      <div>Date</div>
+      <div>{props.date}</div>
       <div className= "expense-item__description">
-        <h2>Car Insurance</h2>
-        <div className="expense-item__price">Amount</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">{props.amount}</div>
       </div>
     </div>
   );
 }
 
 export default ExpenseItem;
+
+
